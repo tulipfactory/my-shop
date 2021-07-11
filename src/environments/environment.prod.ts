8d09310561d89fadf58888a4d85a595dd40ac8ed
Talulah <79481227+tulipfactory@users.noreply.github.com>
2021-07-10 21:35:18 -0500

    fixed something now finished

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBg6jQuagtr0GX5s9o3sZ4isiqhH_PM0yA",
+    authDomain: "talulahstore.firebaseapp.com",
+    projectId: "talulahstore",
+    storageBucket: "talulahstore.appspot.com",
+    messagingSenderId: "371580724992",
+    appId: "1:371580724992:web:f013e6b2ddbfe87221b5e5",
+    measurementId: "G-54D1621QSR"
+  }
 };
